e6fb6a9fb33c214ef3f9c31193569a656ca98b28
francobb <jrizzle1790@gmail.com>
2022-10-11 16:14:19 -0400

    leetcode 122

---
+++ leetcode/editor/en/[122]Best Time to Buy and Sell Stock II.ts
@@ -0,0 +1,66 @@
+//You are given an integer array prices where prices[i] is the price of a given 
+//stock on the iᵗʰ day. 
+//
+// On each day, you may decide to buy and/or sell the stock. You can only hold 
+//at most one share of the stock at any time. However, you can buy it then 
+//immediately sell it on the same day. 
+//
+// Find and return the maximum profit you can achieve. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: prices = [7,1,5,3,6,4]
+//Output: 7
+//Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 
+//5-1 = 4.
+//Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+//
+//Total profit is 4 + 3 = 7.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: prices = [1,2,3,4,5]
+//Output: 4
+//Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 
+//5-1 = 4.
+//Total profit is 4.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: prices = [7,6,4,3,1]
+//Output: 0
+//Explanation: There is no way to make a positive profit, so we never buy the 
+//stock to achieve the maximum profit of 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= prices.length <= 3 * 10⁴ 
+// 0 <= prices[i] <= 10⁴
+// 
+//
+// Related Topics Array Dynamic Programming Greedy 👍 9428 👎 2468
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+function maxProfit(prices: number[]): number {
+  let m = 1;
+  let maxProfit = 0;
+
+  while(m < prices.length) {
+    // if current price is greater than prv price ? add that to value;
+    if (prices[m] > prices[m-1]) maxProfit += prices[m] - prices[m-1];
+    m++
+  }
+
+  return maxProfit;
+};
+//leetcode submit region end(Prohibit modification and deletion)
